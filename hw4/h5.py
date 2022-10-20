a5fcce229cd2cd0deb34ec578dffd258ca137af2
«Andrey» <khomehko@mail.ru>
2022-10-21 02:17:23 +0300

    hw4_h5

--- hw4/h5.py
+++ hw4/h5.py
@@ -1,62 +1,26 @@
 # Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
 
-def get_polynomial(k, ratios):  # вычисляем полином, передаем степень и массив коэффициентов
-    var = ['*x^']*(k-1) + ['*x']
-    polynomial = [[a, b, c] for a, b, c in zip_longest(
-        ratios, var, range(k, 1, -1), fillvalue='') if a != 0]
-    for x in polynomial:
-        x.append(' + ')
-    polynomial = list(chain(*polynomial))
-    polynomial[-1] = ' = 0'
-    return "".join(map(str, polynomial)).replace(' 1*x', ' x')
+with open('h5_1.txt', 'w', encoding='utf-8') as file:
+    file.write('2*x^2 + 5*x^5')
+with open('h5_2.txt', 'w', encoding='utf-8') as file:
+    file.write('23*x^4 + 9*x^6')
+
+with open('h5_1.txt','r') as file:
+    polynom_1 = file.readline()
+    list_polynom_1 = polynom_1.split()
 
 
-from itertools import *
-# import get_polynomial
-import os
-os.system("cls")
+with open('h5_2.txt','r') as file:
+    polynom_2 = file.readline()
+    list_polynom_2 = polynom_2.split()
+
+print(f'Сумма многочленов: {list_polynom_1} + {list_polynom_2}')
+sum_poly = list_polynom_1 + list_polynom_2
+
+with open('h5_summ.txt', 'w', encoding='utf-8') as file:
+    file.write(f' {list_polynom_1} + {list_polynom_2}')
 
 
-file1 = 'h5_1.txt'
-file2 = 'h5_2.txt'
-
-
-def read_pol(file):  # Получение данных из файла
-    with open(str(file), 'r') as data:
-        pol = data.read()
-    return pol
-
-
-def convert_pol(pol):  # удалили "хвостик" и порезали" строку на массив , разделитель знак " + "
-    pol.replace('= 0', '')
-    pol = pol.split(' + ')
-    pol = [i[0] for i in pol]
-    for i in range(len(pol)):
-        if pol[i] == 'x':
-            pol[i] = '1'
-    pol = pol[::-1]
-    return pol
-
-
-pol1 = read_pol(file1)
-pol2 = read_pol(file2)
-print('Исходные полиномы:')
-print(pol1)
-print(pol2)
-print('_'*30)
-print(convert_pol(pol1))
-print(convert_pol(pol2))
-pol1_coef = list(map(int, convert_pol(pol1)))
-pol2_coef = list(map(int, convert_pol(pol2)))
-print(pol1_coef)
-print(pol2_coef)
-print('_'*30)
-
-sum_coef = list(map(sum, zip_longest(pol1_coef, pol2_coef, fillvalue=0)))
-print(sum_coef)
-sum_coef = sum_coef[::-1]
-print(sum_coef)
-sum_pol = get_polynomial(len(sum_coef)-1, sum_coef)
-print('Итоговый результат сложения полиномов:\n', sum_pol)
-with open('task034.txt', 'w') as file_sum:
-    file_sum.writelines(sum_pol)
+# Если я правильно понял, для реального сложения двух многочленов, а не текстовой строки, необходимо в уравнении
+#  оставить только числа в виде строки и затем последовательно складывать получившиеся элементы двух списков,
+#   первый элемент списка 1 с первым элементом списка 2.  Для меня это пока слишком сложно.
